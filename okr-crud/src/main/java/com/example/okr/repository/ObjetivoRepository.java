d70595c5bce7134742155ae77169cbc4dd5472b4
José Pedro <166343703+JP18090@users.noreply.github.com>
2025-04-07 08:32:55 -0300

    Delete okr-crud/src/main directory

--- okr-crud/src/main/java/com/example/okr/repository/ObjetivoRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.okr.repository;
-
-import com.example.okr.model.Objetivo;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ObjetivoRepository extends JpaRepository<Objetivo, Long> {
-}
